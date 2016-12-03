f1796c95db19d6153c80447b3a37b4cab2090714
dangchao123 <693589359@qq.com>
2016-12-03 11:21:50 +0800

    更新

--- 前台静态页/js/show.js
+++ 前台静态页/js/show.js
@@ -1,65 +1,142 @@
-var nn=1;
+// JavaScript Document
 
-var key=0;
+//---------------------------------------------------------------新闻图片切换效果
 
-function change_img(){
-	if(key==0){key=1;
+var str = function (id) {
+	return "string" == typeof id ? document.getElementById(id) : id;
+};
+
+var Class = {
+  create: function() {
+	return function() {
+	  this.initialize.apply(this, arguments);
 	}
-    else if(document.all){
-    	document.getElementById("pic").filters[0].Apply();
-    	document.getElementById("pic").filters[0].Play(duration=2);
-    }
-
-eval('document.getElementById("pic").src=img'+nn+'.src');
-
-eval('document.getElementById("url").href=url'+nn+'.src');
-
-for (var i=1;i<=counts;i++){
-	document.getElementById("xxjdjj"+i).className='axx';
-
+  }
 }
 
-document.getElementById("xxjdjj"+nn).className='bxx';
+Object.extend = function(destination, source) {
+	for (var property in source) {
+		destination[property] = source[property];
+	}
+	return destination;
+}
 
-nn++;
-if(nn>counts){nn=1;}
+var TransformView = Class.create();
+TransformView.prototype = {
+  //容器对象,滑动对象,切换参数,切换数量
+  initialize: function(cont, slider, parameter, count, options) {
+	if(parameter <= 0 || count <= 0) return;
+	var oCont= str(cont), oSlider = str(slider), oThis = this;
 
-tt=setTimeout('change_img()',3000);}
-
-function changeimg(n){
-	nn=n;
-	window.clearInterval(tt);
+	this.Index = 0;//当前索引
 	
+	this._timer = null;//定时器
+	this._slider = oSlider;//滑动对象
+	this._parameter = parameter;//切换参数
+	this._count = count || 0;//切换数量
+	this._target = 0;//目标参数
+	
+	this.SetOptions(options);
+	
+	this.Up = !!this.options.Up;
+	this.Step = Math.abs(this.options.Step);
+	this.Time = Math.abs(this.options.Time);
+	this.Auto = !!this.options.Auto;
+	this.Pause = Math.abs(this.options.Pause);
+	this.onStart = this.options.onStart;
+	this.onFinish = this.options.onFinish;
+	
+	oCont.style.overflow = "hidden";
+	oCont.style.position = "relative";
+	
+	oSlider.style.position = "absolute";
+	oSlider.style.top = oSlider.style.left = 0;
+  },
+  //设置默认属性
+  SetOptions: function(options) {
+	this.options = {//默认值
+		Up:			true,//是否向上(否则向左)
+		Step:		20,//滑动变化率
+		Time:		15,//滑动延时
+		Auto:		true,//是否自动转换
+		Pause:		4000,//停顿时间(Auto为true时有效)
+                onStart:	function(){},//开始转换时执行
+		onFinish:	function(){}//完成转换时执行
+	};
+	Object.extend(this.options, options || {});
+  },
+  //开始切换设置
+  Start: function() {
+	if(this.Index < 0){
+		this.Index = this._count - 1;
+	} else if (this.Index >= this._count){ this.Index = 0; }
+	
+	this._target = -1 * this._parameter * this.Index;
+	this.onStart();
+	this.Move();
+  },
+
+
+  //移动
+  Move: function() {
+	clearTimeout(this._timer);
+	var oThis = this, style = this.Up ? "top" : "left", iNow = parseInt(this._slider.style[style]) || 0, iStep = this.GetStep(this._target, iNow);
+	
+	if (iStep != 0) {
+		this._slider.style[style] = (iNow + iStep) + "px";
+		this._timer = setTimeout(function(){ oThis.Move(); }, this.Time);
+	} else {
+		this._slider.style[style] = this._target + "px";
+		
+		this.onFinish();
+		if (this.Auto) { this._timer = setTimeout(function(){ oThis.Index++; oThis.Start(); }, this.Pause); }
 	}
-	change_img();
-}
+  },
+  //获取步长
+  GetStep: function(iTarget, iNow) {
+	var iStep = (iTarget - iNow) / this.Step;
+	if (iStep >= 0) return 0;
+	if (Math.abs(iStep) < 1) return (iStep > 0 ? 1 : -1);
+	return iStep;
+  },
+  //停止
+  Stop: function(iTarget, iNow) {
+	clearTimeout(this._timer);
+	this._slider.style[this.Up ? "top" : "left"] = this._target + "px";
+  }
+};
 
-document.write('<style>');
+window.onload=function(){
+	function Each(list, fun){
+		for (var i = 0, len = list.length; i < len; i++) { fun(list[i], i); }
+	};
+	
+	var objs = str("idNum").getElementsByTagName("li");
+	
+	var tv = new TransformView("idTransformView", "idSlider", 250, 5, {
+		onStart : function(){ Each(objs, function(o, i){ o.className = tv.Index == i ? "on" : ""; }) }//按钮样式
+	});
+	
+	tv.Start();
+	
+	Each(objs, function(o, i){
+		o.onmouseover = function(){
+			o.className = "on";
+			tv.Auto = false;
+			tv.Index = i;
+			tv.Start();
+		}
+		o.onmouseout = function(){
+			o.className = "";
+			tv.Auto = true;
+			tv.Start();
+		}
+	})
+	
+	////////////////////////test2
+	
+	
 
-document.write('.axx{padding:1px 7px;border-left:#cccccc 1px solid;}');
 
-document.write('a.axx:link,a.axx:visited{text-decoration:none;color:#fff;line-height:12px;font:9px sans-serif;background-color:#666;}');
-
-document.write('a.axx:active,a.axx:hover{text-decoration:none;color:#fff;line-height:12px;font:9px sans-serif;background-color:#999;}');
-
-document.write('.bxx{padding:1px 7px;border-left:#cccccc 1px solid;}');
-
-document.write('a.bxx:link,a.bxx:visited{text-decoration:none;color:#fff;line-height:12px;font:9px sans-serif;background-color:#D34600;}');
-
-document.write('a.bxx:active,a.bxx:hover{text-decoration:none;color:#fff;line-height:12px;font:9px sans-serif;background-color:#D34600;}');
-
-document.write('</style>');
-
-document.write('<div style="width:'+widths+'px;height:'+heights+'px;overflow:hidden;text-overflow:clip;">');
-
-document.write('<div><a id="url" ><img id="pic" style="border:0px;filter:progid:dximagetransform.microsoft.wipe(gradientsize=1.0,wipestyle=4, motion=forward)" width='+widths+' height='+heights+' /></a></div>');
-
-document.write('<div style="filter:background: #ffffff;width:50%-2px;text-align:right;top:-12px;position:relative;margin:1px;height:12px;padding:0px;margin:0px;border:0px;">');
-
-for(var i=1;i<counts+1;i++){document.write('<a href="javascript:changeimg('+i+');" id="xxjdjj'+i+'" class="axx" target="_self">'+i+'</a>');}
-
-document.write('</div></div>');
-
-
-
-change_img();
+	
+}	
