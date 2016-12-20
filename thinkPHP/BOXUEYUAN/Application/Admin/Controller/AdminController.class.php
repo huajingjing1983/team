688dd6583a61b5176a77a1c0c66333e6c6c231c0
shangchengPKU <anshidiandu@foxmail.com>
2016-12-20 23:42:32 +0800

    modified

--- thinkPHP/BOXUEYUAN/Application/Admin/Controller/AdminController.class.php
+++ thinkPHP/BOXUEYUAN/Application/Admin/Controller/AdminController.class.php
@@ -169,10 +169,7 @@ class AdminController extends Controller{
     //     }       
     // }
 
-	// public function __construct(){
- //    	parent::__construct();
- //    
- //    }
+	//
  //    public function add(){
  //        $this->display();
  //    }
